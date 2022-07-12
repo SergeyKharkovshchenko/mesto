fa83c68b8ee9cb0b7ba4ab2969d08d7ea2e44090
Sergey Kharkovshchenko <kharkovchenko@gmail.com>
2022-07-12 12:16:53 +0300

    добавить исправления проектной работы 5 после проверки 2

--- scripts/index.js
+++ scripts/index.js
@@ -18,9 +18,6 @@ const jobElement = document.querySelector('.profile__subtitle');
 const cards = document.querySelector('.elements');
 const cardTemplate = document.querySelector('#card_template').content;
 
-
-
-
 const initialCards = [{
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -50,63 +47,50 @@ const initialCards = [{
 
 
 const handleHeart = function (e) {
-    e.target.closest('.element__heart').classList.toggle('element__heart-color-black');
+    e.target.classList.toggle('element__heart-color-black');
 }
 
 const handleThrashbin = function (e) {
-    // let nameOfLocation = e.target.closest('.element').querySelector('.element__title').textContent;
-    // initialCards.forEach((value, index) => removeFromArray(value.name, nameOfLocation, index));
     e.target.closest('.element').remove();
    
 }
 
 const handleFoto = function (e) {
     openPopup(popupElementForImage);
-    popupTitleElementForImage.textContent = e.target.closest('.element').querySelector('.element__title').textContent;
-    popupFotoElementForImage.src = e.target.closest('.element').querySelector('.element__foto').src;
-    popupFotoElementForImage.alt = e.target.closest('.element').querySelector('.element__title').textContent;; // ? name : initialCards[index].name;
+    const card = e.target.closest('.element')
+    popupTitleElementForImage.textContent = card.querySelector('.element__title').textContent;
+    popupFotoElementForImage.src = card.querySelector('.element__foto').src;
+    popupFotoElementForImage.alt = card.querySelector('.element__title').textContent;; // ? name : initialCards[index].name;
 }
 
 
 function createCard(name, link) {
   // клонируем содержимое тега template
-  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
   // наполняем содержимым
 const foto = cardElement.querySelector('.element__foto');
   foto.src = link; // ? link : initialCards[index].link;
   foto.alt = name; // ? name : initialCards[index].name;
   cardElement.querySelector('.element__title').textContent = name; //? name : initialCards[index].name;
+
+  const likeButtonElement = cardElement.querySelector('.element__heart');
+  likeButtonElement.addEventListener('click', handleHeart);
+  const thrashbinButtonElement = cardElement.querySelector('.element__thrashbin');
+  thrashbinButtonElement.addEventListener('click', handleThrashbin);
+  const fotoButtonElement = cardElement.querySelector('.element__foto');
+  fotoButtonElement.addEventListener('click', handleFoto);
+  
 return cardElement
 }
 
-const addListenersToCard = function (index) {
-  const likeButtonElement = document.querySelectorAll('.element__heart');
-  likeButtonElement[index].addEventListener('click', handleHeart);
-  const thrashbinButtonElement = document.querySelectorAll('.element__thrashbin');
-  thrashbinButtonElement[index].addEventListener('click', handleThrashbin);
-  const fotoButtonElement = document.querySelectorAll('.element__foto');
-  fotoButtonElement[index].addEventListener('click', handleFoto);
-}
-
-  const addCard = function (name, link, index) {
-    let counter = 0;
-    const cardElement = createCard(name, link)
-  // отображаем на странице
+const addCard = function (name, link, index) {
+  const cardElement = createCard(name, link);
   if (index != null) {
-  cards.append(cardElement);
-  addListenersToCard(index);
-   counter++;
+    cards.append(cardElement);
   } else {
     cards.prepend(cardElement);
-    addListenersToCard(counter);
-  
-    // initialCards.unshift({
-    //   name,
-    //   link
-    // });
   }
-
-}
+};
 
 initialCards.forEach((value, index) => addCard(value.name, value.link, index));
 
@@ -119,8 +103,6 @@ const openEditPopup = function () {
 
 const openAddPopup = function (e) {
   openPopup(popupElementForAdd);
-    nameInputForAdd.value = "";
-    jobInputForAdd.value = "";
 }
 
 
@@ -131,17 +113,12 @@ function handleFormEditSubmit(evt) {
   closePopup(popupElementForEdit);
 }
 
-function handleFormAddSubmit(evt) {
-  evt.preventDefault();
+function handleFormAddSubmit(e) {
+  e.preventDefault();
   addCard(nameInputForAdd.value, jobInputForAdd.value);
   closePopup(popupElementForAdd);
+  e.target.reset();
 }
-
-// function removeFromArray(nameInArray, nameInDeletedCard, index) {
-//   if (nameInArray == nameInDeletedCard) {
-//     initialCards.splice(index, 1);
-//   }
-// }
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
@@ -151,15 +128,11 @@ function openPopup(popup) {
   popup.classList.add('popup_opened');
 }
 
-
-
 popupOpenButtonElementForEdit.addEventListener('click', openEditPopup);
 popupOpenButtonElementForAdd.addEventListener('click', openAddPopup);
 
 formInputForEdit.addEventListener('submit', handleFormEditSubmit);
 formInputForAdd.addEventListener('submit', handleFormAddSubmit);
-
-
 
 // находим все крестики проекта по универсальному селектору
 const closeButtons = document.querySelectorAll('.popup__close');
